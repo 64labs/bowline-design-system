1c158eacee8f75bbd2afc6e19f4bdbe7838e44af
Darek Rossman <drossman@64labs.com>
2020-03-27 14:30:13 -0400

    text change

--- docs/src/components/PropsTable.js
+++ docs/src/components/PropsTable.js
@@ -137,7 +137,7 @@ const Prop = ({ prop, name }) => {
               style={{ borderRadius: "3px 0 0 3px" }}
             >
               <Text block size="small">
-                theme
+                tokens
               </Text>
             </Box>
             <Box
@@ -215,8 +215,6 @@ const PropsTable = ({ component }) => {
       <Stack dividers>
         {Object.keys(doc.props).map(propName => {
           const prop = doc.props[propName]
-          const propType = prop.type
-
           return (
             <React.Fragment key={propName}>
               <Prop prop={prop} name={propName} />
@@ -226,119 +224,6 @@ const PropsTable = ({ component }) => {
       </Stack>
     </Stack>
   )
-
-  // return (
-  //   <Columns className="props-table" cols={4}>
-  //     <Column span={1}>
-  //       <Text tone="secondary" size="small">
-  //         Name
-  //       </Text>
-  //     </Column>
-  //     <Column span={1}>
-  //       <Text tone="secondary" size="small">
-  //         Type
-  //       </Text>
-  //     </Column>
-  //     <Column span={1}>
-  //       <Text tone="secondary" size="small">
-  //         Responsive
-  //       </Text>
-  //     </Column>
-  //     <Column span={1}>
-  //       <Text tone="secondary" size="small">
-  //         Description
-  //       </Text>
-  //     </Column>
-  //     <Column span={4}>
-  //       <Divider />
-  //     </Column>
-
-  //     {doc &&
-  //       doc.props &&
-  //       Object.keys(doc.props).map(propName => {
-  //         const prop = doc.props[propName]
-
-  //         const propValues =
-  //           prop.type &&
-  //           prop.type.name !== "union" &&
-  //           (tokens[
-  //             prop.type.value &&
-  //               !Array.isArray(prop.type.value) &&
-  //               prop.type.value.replace("types.", "").replace("tokens.", "")
-  //           ] ||
-  //             tokens[
-  //               prop.type.raw &&
-  //                 prop.type.raw.replace("types.", "").replace("tokens.", "")
-  //             ] ||
-  //             prop.type.value)
-
-  //         const propOneOf =
-  //           prop.type &&
-  //           prop.type.name === "union" &&
-  //           prop.type.value.find(v => v.name === "arrayOf")
-
-  //         return (
-  //           <React.Fragment key={propName}>
-  //             <Column span={1}>
-  //               <Text weight="medium" size="small">
-  //                 {propName}
-  //               </Text>
-  //             </Column>
-  //             <Column span={1}>
-  //               {propValues ? (
-  //                 <Inline space="smallish">
-  //                   {propValues.map(val => (
-  //                     <Text
-  //                       as="code"
-  //                       key={`${propName}-${val.value || val}`}
-  //                       baseline={false}
-  //                       size="small"
-  //                     >
-  //                       {val.value ? val.value.replace(/'/g, "") : val}
-  //                     </Text>
-  //                   ))}
-  //                 </Inline>
-  //               ) : (
-  //                 <Text size="small">
-  //                   {prop.type && prop.type.name === "union"
-  //                     ? propOneOf
-  //                       ? propOneOf.value.name
-  //                       : prop.type.value.map(v => v.name).join(", ")
-  //                     : prop.type && prop.type.name}
-  //                 </Text>
-  //               )}
-  //             </Column>
-  //             <Column span={1}>
-  //               {prop.type && (prop.type.name === "custom" || propOneOf) ? (
-  //                 <Box paddingLeft="gutter">
-  //                   <Icon name="check" />
-  //                 </Box>
-  //               ) : null}
-  //             </Column>
-  //             <Column span={1}>
-  //               <Stack space="small">
-  //                 {prop.description && (
-  //                   <Text size="small">{prop.description}</Text>
-  //                 )}
-  //                 {prop.defaultValue &&
-  //                   prop.defaultValue.value !== "undefined" && (
-  //                     <Text size="small">
-  //                       Default:{" "}
-  //                       <Text as="code" size="small" baseline={false}>
-  //                         {prop.defaultValue.value.replace(/'/g, "")}
-  //                       </Text>
-  //                     </Text>
-  //                   )}
-  //               </Stack>
-  //             </Column>
-  //             <Column span={4}>
-  //               <Divider />
-  //             </Column>
-  //           </React.Fragment>
-  //         )
-  //       })}
-  //   </Columns>
-  // )
 }
 
 export default PropsTable
